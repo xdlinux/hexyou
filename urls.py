a036d2e1d6c3abd69d857fa4e3708a085cf72ce2
shanzi <yun.er.run@gmail.com>
2011-12-03 11:56:34 +0800

    配置静态文件和media的访问路径

--- urls.py
+++ urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls.static import static
 
 # Uncomment the next two lines to enable the admin:
  from django.contrib import admin
@@ -15,3 +16,5 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
      url(r'^admin/', include(admin.site.urls)),
 )
+urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT )
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT )
