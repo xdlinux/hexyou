a2772fc29016749bb88677561f415f3072aace9a
shanzi <yun.er.run@gmail.com>
2011-12-03 11:53:40 +0800

    将profile剥离到一个新的app中

--- activity/models.py
+++ activity/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Location(models.Model):
+	name = models.CharField(max_length=30)
+
 class Activity(models.Model):
 	name = models.CharField(max_length=30)
 	date = models.DateField()
@@ -9,12 +12,6 @@ class Activity(models.Model):
 	description = models.TextField(null=True, blank=True)
 	location = models.ForeignKey(Location)
 	host = models.ForeignKey(User)
+    host_group = models.ForeignKey(Group,null=True)
 	participator = models.ManyToManyField(User)
 
-class Profile(models.Model):
-	user = models.OneToOne(User)
-	group = models.BooleanField()
-	verified = models.BooleanField()
-
-class Location(models.Model):
-	name = models.CharField(max_length=30)
