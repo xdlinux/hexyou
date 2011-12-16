6a52b30bd0be901c821bec956375a9da6ea0e0a0
yinian1992 <yinian1992@gmail.com>
2011-12-16 20:56:41 +0800

    add some blocks in base template & accomplish group frontpage template

--- group/views.py
+++ group/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render_to_response
 
 def frontpage(requset):
     """docstring for group"""
-    return render_to_response('group/base.html')
+    return render_to_response('group/frontpage.html')
