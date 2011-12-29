847d9ad93b24b9dad8e676283470fee72c3e6c27
yinian1992 <yinian1992@gmail.com>
2011-12-29 23:22:19 +0800

    大部分调整，加入顶部动作栏，members/single界面占位完成

--- activity/views.py
+++ activity/views.py
@@ -1,1 +1,8 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+
+def frontpage(requset):
+    """docstring for group"""
+    return render_to_response('activities/frontpage.html')
+
+def single(request):
+    return render_to_response('activities/single.html')
