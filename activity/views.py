dadc78a62353ed70be526f58ac5f048f01987d13
yinian1992 <yinian1992@gmail.com>
2012-01-31 23:27:56 +0800

    添加groups/admin，细节变动

--- activity/views.py
+++ activity/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render_to_response,redirect
 from django.template import RequestContext
 from NearsideBindings.activity.forms import ActivityForm
-from NearsideBindings.activity.models import Location
+from NearsideBindings.activity.models import *
 from django.contrib.auth.decorators import login_required
 from django.template import RequestContext
 from django.http import HttpResponse
@@ -9,7 +9,9 @@ from django.http import HttpResponse
 
 def frontpage(request):
     """docstring for group"""
-    return render_to_response('activities/frontpage.html')
+    types = ActivityType.objects.all()
+    location_roots = Location.objects.filter(parent=0)
+    return render_to_response('activities/frontpage.html',locals())
 
 def single(request):
     return render_to_response('activities/single.html')
--- group/views.py
+++ group/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render_to_response,redirect,get_object_or_404
 from NearsideBindings.group.forms import GroupForm
-from NearsideBindings.group.models import Group
+from NearsideBindings.group.models import Group,MemberShip
 from django.contrib.auth.decorators import login_required
 from django.template import RequestContext
 
 
+@login_required(login_url='/login/')
 def frontpage(request):
     """docstring for group"""
-    return render_to_response('groups/frontpage.html')
+    managed_groups = [ membership.group for membership in MemberShip.objects.filter(user=request.user,is_admin=True) ]
+    managed_counter = len(managed_groups)
+    joined_groups = [ membership.group for membership in MemberShip.objects.filter(user=request.user,is_admin=False) ]
+    joined_counter = len(joined_groups)
+    return render_to_response('groups/frontpage.html', locals())
 
+@login_required(login_url='/login/')
 def single(request,group_slug):
     group = get_object_or_404(Group,slug=group_slug)
     return render_to_response('groups/single.html',locals())
@@ -26,4 +32,7 @@ def new(request):
         'form':form
         }, context_instance=RequestContext(request)
     )
-    
+
+@login_required(login_url='/login/')
+def admin(request,group_slug):
+    return render_to_response('groups/admin.html')
