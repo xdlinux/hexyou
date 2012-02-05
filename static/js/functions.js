d7a064b174513d780956ccc6278b8e8aff6f6c4f
shanzi <yun.er.run@gmail.com>
2012-02-05 16:22:41 +0800

    更改holder，focus的时候消失，blur的时候显示，如果input包含字段，则不显示

--- static/js/functions.js
+++ static/js/functions.js
@@ -41,18 +41,16 @@ $(document).ready(function(){
   var holding = $('form>.holding>input').filter('input[type=text],input[type=email],input[type=password]');
   holding.each(function(){
     var ts=$(this);
+    if(ts.val()!='')ts.siblings('.holder').hide();
     ts.siblings('.holder').click(function(){ts.focus()});
-    if (ts.val()!=''){
-      ts.siblings('.holder').hide(200);
-    }
   })
-  holding.keypress(function(){
+  holding.focus(function(){
     $(this).siblings('.holder').hide(200);
-  }).change(function(){
+  }).blur(function(){
     if($(this).val()==''){
       $(this).siblings('.holder').show();
     }
-  })
+  });
   $('input[rel=popover]').popover({'trigger':'focus','offset':32});
   
 /* modal */
