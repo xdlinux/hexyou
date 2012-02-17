42e62aef7afd384e083b668ec2c25cccc79e5fcf
shanzi <yun.er.run@gmail.com>
2012-02-17 17:49:21 +0800

    migrations

---
+++ activity/migrations/0013_auto__add_field_activityphoto_thumbnail.py
@@ -0,0 +1,150 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Adding field 'ActivityPhoto.thumbnail'
+        db.add_column('activity_activityphoto', 'thumbnail', self.gf('django.db.models.fields.CharField')(default='', max_length=128, blank=True), keep_default=False)
+
+
+    def backwards(self, orm):
+        
+        # Deleting field 'ActivityPhoto.thumbnail'
+        db.delete_column('activity_activityphoto', 'thumbnail')
+
+
+    models = {
+        'activity.activity': {
+            'Meta': {'object_name': 'Activity'},
+            'activity_type': ('django.db.models.fields.related.ForeignKey', [], {'default': '1', 'to': "orm['activity.ActivityType']"}),
+            'avatar': ('django.db.models.fields.CharField', [], {'default': "'/static/images/no_avatar.png'", 'max_length': '100'}),
+            'begin_time': ('django.db.models.fields.DateTimeField', [], {}),
+            'description': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'end_time': ('django.db.models.fields.DateTimeField', [], {}),
+            'host_groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['group.Group']", 'symmetrical': 'False', 'through': "orm['activity.HostShip']", 'blank': 'True'}),
+            'host_string': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'location': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['activity.Location']"}),
+            'members': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "'participated_activities'", 'blank': 'True', 'through': "orm['activity.MemberHostShip']", 'to': "orm['auth.User']"}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '30'})
+        },
+        'activity.activityphoto': {
+            'Meta': {'object_name': 'ActivityPhoto'},
+            'activity': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'photos'", 'to': "orm['activity.Activity']"}),
+            'description': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'source': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'thumbnail': ('django.db.models.fields.CharField', [], {'max_length': '128', 'blank': 'True'})
+        },
+        'activity.activitytype': {
+            'Meta': {'object_name': 'ActivityType'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '20'})
+        },
+        'activity.hostship': {
+            'Meta': {'unique_together': "(('group', 'activity'),)", 'object_name': 'HostShip'},
+            'accepted': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'activity': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['activity.Activity']"}),
+            'group': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['group.Group']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
+        },
+        'activity.location': {
+            'Meta': {'object_name': 'Location'},
+            'full_path': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'}),
+            'parent': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['activity.Location']", 'null': 'True', 'blank': 'True'})
+        },
+        'activity.memberhostship': {
+            'Meta': {'unique_together': "(('user', 'activity'),)", 'object_name': 'MemberHostShip'},
+            'activity': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['activity.Activity']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_host': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"})
+        },
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'avatar': ('django.db.models.fields.CharField', [], {'max_length': '100', 'blank': 'True'}),
+            'bio': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'contact_fanfou': ('django.db.models.fields.CharField', [], {'max_length': '75', 'blank': 'True'}),
+            'contact_msn': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'contact_qq': ('django.db.models.fields.CharField', [], {'max_length': '20', 'blank': 'True'}),
+            'contact_twitter': ('django.db.models.fields.CharField', [], {'max_length': '32', 'blank': 'True'}),
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'site': ('django.db.models.fields.URLField', [], {'max_length': '200', 'blank': 'True'}),
+            'student_num': ('django.db.models.fields.CharField', [], {'max_length': '8', 'blank': 'True'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'group.condition': {
+            'Meta': {'object_name': 'Condition'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '20', 'blank': 'True'})
+        },
+        'group.group': {
+            'Meta': {'object_name': 'Group'},
+            'avatar': ('django.db.models.fields.CharField', [], {'default': "'/static/images/no_avatar.png'", 'max_length': '512'}),
+            'condition': ('django.db.models.fields.related.ForeignKey', [], {'default': '1', 'to': "orm['group.Condition']"}),
+            'create_date': ('django.db.models.fields.DateField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'founder': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'founder'", 'to': "orm['auth.User']"}),
+            'friend_groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['group.Group']", 'symmetrical': 'False'}),
+            'group_type': ('django.db.models.fields.related.ForeignKey', [], {'default': '1', 'to': "orm['group.GroupType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'members': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.User']", 'through': "orm['group.MemberShip']", 'symmetrical': 'False'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '20'}),
+            'site': ('django.db.models.fields.URLField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '50', 'db_index': 'True'})
+        },
+        'group.grouptype': {
+            'Meta': {'object_name': 'GroupType'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '20'})
+        },
+        'group.membership': {
+            'Meta': {'unique_together': "(('group', 'user'),)", 'object_name': 'MemberShip'},
+            'group': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['group.Group']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_admin': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_approved': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'joined_date': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"})
+        }
+    }
+
+    complete_apps = ['activity']
